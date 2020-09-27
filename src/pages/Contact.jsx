f37b1554ab0fcc29801cdbdeeafe853e1cd4d261
Halkim44 <kimketchum15@gmail.com>
2020-09-27 23:04:25 +0800

    change to default export on lazy components

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export const Contact = () => {
+export default () => {
   return (
     <div>
       Contact
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export const Projects = () => {
+export default () => {
   return (
     <div>
       Projects
